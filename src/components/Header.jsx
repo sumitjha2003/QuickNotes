3e7998d4165903f206428350017684b821d71b00
Sumit Jha <101612853+sumitjha2003@users.noreply.github.com>
2023-06-18 11:12:59 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AddCircleOutlineTwoToneIcon from '@material-ui/icons/AddCircleOutlineTwoTone';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AddCircleOutlineTwoToneIcon fontSize="large"/>
+        QuickNotes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
